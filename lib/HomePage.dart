82adf3de3ca83f35ee4ef0946e951f941e843556
Anmol KC <kcanmol388@gmail.com>
2023-07-28 21:41:29 +0545

    navigation done to editing page from homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ionicons/ionicons.dart';
-import 'package:provider/provider.dart'; // Import the flutter_icons library
+import 'package:noteapp/models/data.dart';
+import 'package:noteapp/pages/Edit_NotePage.dart';
+import 'package:provider/provider.dart';
+
+import 'models/Note.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,64 +16,122 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //creating new note
+  void createNewNote() {
+    //creating the new id
+    int id = Provider.of<Notedata>(context, listen: false).getAllNotes().length;
+    
+    //
+    Note newNote = Note(
+      id: id,
+      text: '',
+    );
+
+    //Go to Edit the note
+    goToNotePage(newNote, true);
+  }
+
+  //Go to note editing
+  void goToNotePage(Note note, bool isNewNote){
+    Navigator.push(
+      context,
+     MaterialPageRoute(
+      builder: (context) => EditingNotePage(
+        note: note,
+        isNewNote: false,
+      ),
+      ),
+      );
+  }
+
+  //Delete the note
+  void deleteNote(Note note){
+    Provider.of<Notedata>(context, listen: false).deleteNode(note);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (context , value, child) => Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50),
-        child: AppBar(
-          title: Text(
-            'Notes',
-            style: GoogleFonts.roboto(
-              textStyle: const TextStyle(
-                color: Colors.black,
-                fontSize: 23,
-                fontWeight: FontWeight.bold,
+    return Consumer<Notedata>(
+      builder: (context, value, child) => Scaffold(
+        backgroundColor: CupertinoColors.systemGroupedBackground,
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: createNewNote,
+        //   child: Icon(Ionicons.add),
+        // ),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: AppBar(
+            title: Text(
+              'Notes',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
+            centerTitle: true, // Center the title within the AppBar
+            backgroundColor: Colors.white,
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(right: 12.0),
+                child: Icon(
+                  Ionicons.ellipsis_horizontal_circle_outline,
+                  size: 30, // Replace this with the Ionicon you want to use
+                  color: Color.fromARGB(255, 238, 204, 104),
+                  // Change the color of the icon as needed
+                ),
+              ),
+            ],
           ),
-          centerTitle: true, // Center the title within the AppBar
-          backgroundColor: Colors.white,
-          actions: [
-             Padding(
-              padding: EdgeInsets.only(right: 12.0),
-              child: Icon(
-                Ionicons.ellipsis_horizontal_circle_outline,
-                size: 30, // Replace this with the Ionicon you want to use
-                color: Color.fromARGB(255, 238, 204, 104),
-                 // Change the color of the icon as needed
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 25.0, top: 25),
+              child: Text(
+                'Notes',
+                style: GoogleFonts.roboto(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            // List of Notes
+            CupertinoListSection.insetGrouped(
+              children: List.generate(
+                value.getAllNotes().length,
+                (index) => CupertinoListTile(
+                  title: Text(value.getAllNotes()[index].text),
+                ),
               ),
             ),
           ],
         ),
-        
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Text('Notes',
-            style: GoogleFonts.roboto(
-              textStyle: const TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white, // Set the background color to match the app bar
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {
+                  createNewNote();
+                },
+                icon: Icon(
+                  Ionicons.add_circle_outline, // Replace this with your desired additional icon
+                  size: 32.0, // Adjust the size of the additional icon
+                  color: const Color.fromARGB(255, 238, 204, 104), // Change the color of the additional icon
+                ),
               ),
-            ),
-            ),
+            ],
           ),
-        //List of Notes 
-
-        CupertinoListSection(
-          children:[
-            CupertinoListTile(title: Text('Hello'),)
-          ],
         ),
-
-
-        ],
       ),
-    )
-    );  
+    );
   }
 }
---
+++ lib/pages/Edit_NotePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../models/Note.dart';
+
+class EditingNotePage extends StatefulWidget {
+  Note note;
+  bool isNewNote;
+  EditingNotePage({super.key, 
+  required this.note,
+   required this.isNewNote});
+
+  @override
+  State<EditingNotePage> createState() => _EditingNotePageState();
+}
+
+class _EditingNotePageState extends State<EditingNotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
